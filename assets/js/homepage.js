22f3beef3fc18a885b673f0b5d88fb4322f23085
dannyrgd <danny.ramirezgd@gmail.com>
2021-12-21 12:23:55 -0500

    captured weather info, added dynamic elements, setting local storage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,46 +1,66 @@
 var cityInputEl = document.querySelector("#city-name");
 var stateInputEl = document.querySelector("#states");
-//fetch data from weather api by lat lon and exclude any parts?
+var searchBtnEl = document.querySelector("#search-btn");
+var select = document.getElementById("states");
+var cityInput = document.getElementById("city-name");
+var focusCityEl = document.getElementById("focus-city");
+var cityDescriptors = document.getElementById("city-descriptors");
+
+//grabbing input from form and dropdown and pulling the lat and lon
 var getLocationInfo = function(city, state){
-    var cityInput = cityInputEl.textContent
-    var stateInput = stateInputEl.value
-    console.log(city);
-    console.log(state);
     var apiUrl = "http://api.geonames.org/searchJSON?q=" + city + "&adminCode1=" + state + "&maxRows=10&username=dannyramirezgd"
-    
 
     fetch(apiUrl).then(function(response){
         if(response.ok){
             response.json().then(function(data){
-                console.log(data);
+                var lat = (data.geonames[0].lat)
+                var lon = (data.geonames[0].lng)
+                getWeatherInfo(lat, lon);
             });
         }else{
             alert("Error")
         }
     })
 }
+//get info from weather API based on lat and lon of city from getLocationInfo function
 var getWeatherInfo = function (lat, lon){
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon=" + lon +"&units=imperial&appid=56d05661cb74c3389542f3c94dddc04e"
 
     fetch(apiUrl).then(function(response){
         if(response.ok){
             response.json().then(function(data){
-                console.log(data);
+                localStorage.setItem("weather", JSON.stringify(data));
             });
         } else {
             alert("Error: Did not receive response");
         }
     })
 }
-//create a function on search
-//have the input form take in City
-//create a drop down for State
-//pull content from each and turn into string and push into "city" & "state" getLocationInfo function
-    // is there a way to search up cities and be given their lat and lon? 
+//create when button is clicked create city and state info for location and weather info functions
+var searchButtonHandler = function (event){
+    event.preventDefault()
+    var value = select.options[select.selectedIndex].value;
+    var cityName = cityInput.value;
+    getLocationInfo(cityName, value)
+    createFocusedCity();
+}
+var createFocusedCity = function(){
+    var getWeatherInfo = JSON.parse(localStorage.getItem("weather"));
+    console.log(getWeatherInfo.current.temp);
+    focusCityEl.textContent = "";
+    focusCityEl.textContent = cityInput.value + " , " + select.options[select.selectedIndex].value; 
+    var tempEl = document.createElement("li");
+    tempEl.textContent = "Current Temp: ";
+    var windEl = document.createElement("li");
+    var uviEl = document.createElement("li");
+    //focusCityEl.appendChild(cityNameEl);
+}
+var createFutureCity = function(){
+    //this is where the future forecast will go
+}
 //after search is made add info as button into list under the form into local storage
 //local storage should be the last city searched
-getWeatherInfo("20.45","-40.18");
-getLocationInfo(cityInput, stateInput)
+
 //the info I need
 //current weather conditions
     //object.current.weather[0].main describes the weather icon?
@@ -66,3 +86,4 @@ getLocationInfo(cityInput, stateInput)
     //object.current.wind_speed
 //uv index
     //object.current.uvi
+searchBtnEl.addEventListener("click", searchButtonHandler);
